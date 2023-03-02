9de9862d91046a4b85c6917da7eb7ff1448682c0
HS-11 <lloot60@gmail.com>
2023-03-02 10:07:23 +0400

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,64 @@
+function App() {
+  return (
+    <div className="min-h-screen flex flex-col text-white">
+      <main className="container mx-auto px-6 pt-16 flex-1 text-center">
+
+          <h2 className="text-2xl.md:text-4xl.lg:text-6xl uppercase">
+            Welcome to
+          </h2>
+
+          <h1 className="text-3xl md:text-6xl lg:text-8xl font-black uppercase mb-8">
+            Uae Coders
+          </h1>
+
+
+        <div className="text-lg md:text-2xl lg:text-3xl py-2 px-4 md:py-4 md:px-10 lg:py-6 lg:px-12 bg-white bg-opacity-10 w-fit mx-auto mb-8 rounded-full">
+          There are more than 63,000 programmers all across the UAE !
+        </div>
+
+        <form 
+					action="https://ai.gov.ae/codershq/" 
+					method="post" 
+					id="revue-form"
+					name="revue-form"
+					target="_blank">
+
+					<div className="flex flex-col md:flex-row justify-center mb-4">
+						<input
+							placeholder="Your email address"
+							type="email"
+							name="member[email]"
+							id="member_email"
+							className="text-2xl placeholder:text-gray-400 placeholder:italic py-4 px-6 md:px-10 lg:py-6 lg:px-12 bg-white bg-opacity-10 focus:bg-opacity-20 duration-150 md:rounded-tr-none md:rounded-br-none rounded-full outline-none mb-4 md:mb-0"
+						/>
+						<input
+							type="submit"
+							value="Join Today"
+							name="member[subscribe]"
+							id="member_submit"
+							className="bg-primary md:rounded-tl-none md:rounded-bl-none rounded-full text-2xl py-4 px-6 md:px-10 lg:py-6 lg:px-12 font-bold uppercase cursor-pointer hover:opacity-75 duration-150"
+						/>
+					</div>
+
+          
+
+				</form>
+
+      </main>
+
+      <footer className="container mx-auto p-6 flex flex-col md:flex-row items-center justify-between">
+        <p>Buit by Helal Salem</p>
+
+        <div className="flex -mx-6">
+          <a href="#" className="mx-3 hover:opacity-80 duratuion-150">About us</a>|
+          <a href="" className="mx-3 hover:opacity-80 duratuion-150">Contact</a>
+
+        </div>
+
+      </footer>
+    </div>
+
+  )
+}
+
+export default App
