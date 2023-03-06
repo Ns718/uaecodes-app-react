c20d83eb914547bdf2a99f7a6f83d89ad4ebf782
HS-11 <lloot60@gmail.com>
2023-03-06 22:51:31 +0400

    deploy

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base:'/uaecode-app-react/'
+  base:'/uaecodes-app-react/'
 })
